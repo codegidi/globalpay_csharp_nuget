f4ca2d27a182d2b9048c01ee97de0600baf3d43b
Mayowa Anibaba <mayowa.anibaba@gmail.com>
2018-12-18 22:36:48 +0100

    Updated URL on staging and live

--- GlobalPay/Net.Globals/Constants.cs
+++ GlobalPay/Net.Globals/Constants.cs
@@ -6,11 +6,13 @@ namespace GlobalPay.Net.Globals
 {
     public class Constants
     {
-        public const string BaseEndURlLive = "http://globalpay.azurewebsites.net";
-        public const string BaseEndURlStaging = "http://globalpayapi.azurewebsites.net";
-        public const string AuthURLStaging = "http://globalpayauthserver.azurewebsites.net";
-        public const string AuthURLLive = "http://globalpayauthserver.azurewebsites.net";
+        public const string BaseEndURlLive = "https://api.globalpay.com.ng/v2/PaymentGateway/PaymentGatewayCapture";
+        public const string BaseEndURlStaging = "https://gpaygatewayapi.azurewebsites.net/v2/PaymentGateway/PaymentGatewayCapture";
+        public const string AuthURLStaging = "https://gpayauthorisation.azurewebsites.net/";
+        public const string AuthURLLive = "https://auth.globalpay.com.ng";
         public const string ContentTypeHeaderJson = "application/json";
         public const string AuthorizationHeaderType = "Bearer";
     }
 }
+
+
