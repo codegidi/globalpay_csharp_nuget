3524a13b603eb128dae1dfe3db00597761fc63f9
Mayowa Anibaba <mayowa.anibaba@gmail.com>
2018-12-21 14:40:19 +0100

    Global pay

--- GlobalPay/Net.Globals/Constants.cs
+++ GlobalPay/Net.Globals/Constants.cs
@@ -6,10 +6,10 @@ namespace GlobalPay.Net.Globals
 {
     public class Constants
     {
-        public const string BaseEndURlLive = "https://api.globalpay.com.ng/v2/PaymentGateway/PaymentGatewayCapture";
-        public const string BaseEndURlStaging = "https://gpaygatewayapi.azurewebsites.net/v2/PaymentGateway/PaymentGatewayCapture";
-        public const string AuthURLStaging = "https://gpayauthorisation.azurewebsites.net/";
-        public const string AuthURLLive = "https://auth.globalpay.com.ng";
+        public const string BaseEndURlLive = "https://auth.globalpay.com.ng";
+        public const string BaseEndURlStaging = "https://gpaygatewayapi.azurewebsites.net";
+        public const string AuthURLStaging = "https://gpayauthorisation.azurewebsites.net/connect/token";
+        public const string AuthURLLive = "https://auth.globalpay.com.ng/connect/token";
         public const string ContentTypeHeaderJson = "application/json";
         public const string AuthorizationHeaderType = "Bearer";
     }
