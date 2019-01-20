d4f5eca50fa109ed4faf727d9c08e19a94b8d0b6
Codegidi <mayowa.anibaba@gmail.com>
2019-01-20 18:33:40 +0100

    added base url call to lib

--- GlobalPay/Transactions.cs
+++ GlobalPay/Transactions.cs
@@ -6,36 +6,51 @@ using GlobalPay.Net.Globals;
 using GlobalPay.Net.Interfaces;
 using GlobalPay.Net.Models;
 using System.Collections.Generic;
+using System;
 
 namespace GlobalPay.Net
 {
-    public class GlobalPayTransactions : ITransaction {
+    public class GlobalPayTransactions : ITransaction
+    {
 
         private string _token;
 
-        public GlobalPayTransactions(string token) {
+        public static string baseURL = Constants.BaseEndURlStaging;
+
+        public GlobalPayTransactions(string token)
+        {
             this._token = token;
         }
 
-        public async Task<TransactionRegistrationResponse> InitializeTransaction(string _returnurl, string _merchantreference, string _merchantid, string _description, string _totalamount, string _currencycode, string _customerEmail, string _customerNumber, string _customerFirstName, string _customerLastName, bool isLive) {
+        public async Task<TransactionRegistrationResponse> InitializeTransaction(string _returnurl, string _merchantreference, string _merchantid, string _description, string _totalamount, string _currencycode, string _customerEmail, string _customerNumber, string _customerFirstName, string _customerLastName, bool isLive)
+        {
             var client = HttpConnection.Call(_token, isLive);
+
+            if (isLive)
+            {
+                baseURL = Constants.BaseEndURlLive;
+            }
+
             List<Product> _products = new List<Product>();
 
-            var _product = new Product {
+            var _product = new Product
+            {
                 Name = _description,
                 Quantity = "1",
                 Unitprice = _totalamount
             };
             _products.Add(_product);
 
-            var _customer = new Customer {
+            var _customer = new Customer
+            {
                 Email = _customerEmail,
                 Firstname = _customerFirstName,
                 Lastname = _customerLastName,
                 Mobile = _customerNumber,
             };
 
-            var transactionRegistrationRequest = new TransactionRegistrationRequest {
+            var transactionRegistrationRequest = new TransactionRegistrationRequest
+            {
                 Returnurl = _returnurl,
                 Customerip = "",
                 Merchantreference = _merchantreference,
@@ -53,15 +68,22 @@ namespace GlobalPay.Net
 
             var requestJson = JsonConvert.SerializeObject(transactionRegistrationRequest);
             var stringContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("/SetRequest", stringContent);
+            var response = await client.PostAsync(baseURL + "/SetRequest", stringContent);
             var responseJson = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<TransactionRegistrationResponse>(responseJson);
         }
 
-        public async Task<RetrieveTransactionResponse> RetrieveTransaction(string _merchantId, string _merchantReference, string _transactionReference, bool isLive) {
+        public async Task<RetrieveTransactionResponse> RetrieveTransaction(string _merchantId, string _merchantReference, string _transactionReference, bool isLive)
+        {
             var client = HttpConnection.Call(_token, isLive);
-            var _retrieveTransactionRequest = new RetrieveTransactionRequest {
+            if (isLive)
+            {
+                baseURL = Constants.BaseEndURlLive;
+            }
+
+            var _retrieveTransactionRequest = new RetrieveTransactionRequest
+            {
                 Merchantid = _merchantId,
                 Merchantreference = _merchantReference,
                 Transactionreference = _transactionReference,
@@ -69,7 +91,7 @@ namespace GlobalPay.Net
 
             var requestJson = JsonConvert.SerializeObject(_retrieveTransactionRequest);
             var stringContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("/Retrieve", stringContent);
+            var response = await client.PostAsync(baseURL + "/Retrieve", stringContent);
             var responseJson = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<RetrieveTransactionResponse>(responseJson);
